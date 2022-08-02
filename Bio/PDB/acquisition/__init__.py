6dda613ed758f281ecb0a45db7adc32ee95d44ba
JulianMaurin <julian.maurin.dev@pm.me>
2022-08-02 23:06:30 +0200

    feat(Bio.PDB): introduce acquisition package

---
+++ Bio/PDB/acquisition/__init__.py
@@ -0,0 +1,16 @@
+"""
+Protein Data Bank (PDB) acquisition package (Bio.PDB.acquisition).
+
+This package respects python module naming convention (see: https://peps.python.org/pep-0008/#package-and-module-names).
+
+Materials:
+- https://www.wwpdb.org/ftp/pdb-ftp-sites
+- https://www.rcsb.org/docs/programmatic-access/file-download-services
+
+NOTE: only export the object consumed outside of the module.
+"""
+
+# Copyright (C) 2022, Julian Maurin (julian.maurin.dev@proton.me)
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
